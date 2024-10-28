096ecacb9dc842c8e0b59d700b3d9b3d7b5c089d
Astha241105 <astha241105@gmail.com>
2024-10-28 11:06:06 +0530

    added otp and forgot password pages with proper dimensions

--- src/otpWithMail.jsx
+++ src/otpWithMail.jsx
@@ -4,15 +4,15 @@ import './otpWithMail.css'
 const OtpWithMail = () => {
   return (
       <div id="container1">
-      <img src=""></img>
-      <h1>Enter the code</h1>
-    <p>Enter the OTP we have sent to<br></br> abc@gmail.com.<br></br> Be careful not to share the code with anyone.</p>
-      <input></input>
-      <input></input>
-      <input></input>
-      <input></input><br></br>
-      <button>Verify</button><br></br>
-      <span>Didn't receive the code?</span><span id="gradient"> Resend code.</span></div>
+      <div id="codemail">Enter the code</div>
+    <p id="mess1">Enter the OTP we have sent to<br></br> abc@gmail.com.<br></br> Be careful not to share the code with anyone.</p>
+      <input className="boxes"></input>
+      <input className="boxes"></input>
+      <input className="boxes"></input>
+      <input className="boxes"></input><br></br>
+      <button className="verify1">Verify</button><br></br>
+      <div className="didnt1">
+      <span >Didn't receive the code?</span><span id="gradient1"> Resend code.</span></div></div>
   )
 }
 
--- src/otpWithphone.jsx
+++ src/otpWithphone.jsx
@@ -4,15 +4,16 @@ import './otpWithPhone.css'
 const OtpWithPhone = () => {
   return (
       <div id="container2">
-      <img src=""></img>
-      <h1>Enter the code</h1>
-    <p>Enter the OTP we have sent to<br></br> XXXXXXX521<br></br> Be careful not to share the code with anyone.</p>
-      <input></input>
-      <input></input>
-      <input></input>
-      <input></input><br></br>
-      <button>Verify</button><br></br>
-      <span>Didn't receive the code?</span><span id="gradient"> Resend code.</span></div>
+      <h1 id="codephone">Enter the code</h1>
+    <p id="mess2">Enter the OTP we have sent to<br></br> XXXXXXX521<br></br> Be careful not to share the code with anyone.</p>
+      <input className="boxes2"></input>
+      <input className="boxes2"></input>
+      <input className="boxes2"></input>
+      <input className="boxes2"></input>
+      <br></br>
+      <button id="verify2">Verify</button><br></br>
+      <div className="didnt2">
+      <span>Didn't receive the code?</span><span id="gradient2"> Resend code.</span></div></div>
   )
 }
 
