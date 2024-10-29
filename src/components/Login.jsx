ba3aff26a9068497b72b18c69f2c3303527b6ba3
gtanishka100 <gtanishka100@gmail.com>
2024-10-29 11:40:51 +0530

    corrected img path as root level path

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -25,14 +25,10 @@ const Login = () => {
 
   return (
     <div className="login">
-      <img
-        src="../../public/Rectangle2.png"
-        className="white-bg"
-        alt="background"
-      />
+      <img src="/Rectangle2.png" className="white-bg" alt="background" />
       <div className="container">
         <div className="image-section">
-          <img src="../../public/side1.png" alt="logo" className="logo" />
+          <img src="/side1.png" alt="logo" className="logo" />
         </div>
         <div className="form-section">
           <h3>Welcome Back!</h3>
@@ -40,6 +36,7 @@ const Login = () => {
             <div className="form-group">
               <label>Username:</label>
               <input
+                className="input-box"
                 type="text"
                 name="username"
                 placeholder="Enter username"
@@ -51,6 +48,7 @@ const Login = () => {
             <div className="form-group">
               <label>Password:</label>
               <input
+                className="input-box"
                 type="password"
                 name="password"
                 placeholder="Enter password"
