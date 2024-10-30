aeb97a9fe533d26a237afecc41091ca93550c2c9
gtanishka100 <gtanishka100@gmail.com>
2024-10-31 01:10:41 +0530

    Added create password page

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export { default as CreateAccountP } from "./CreateAccountP/CreateAccountP";
 export { default as ForgotPassword } from "./ForgetPassword/ForgotPassword";
 export { default as CreateAccountH } from "./CreateAccountH/CreateAccountH";
 export { default as Org_Des } from "./CreateAccountH/Org_Des";
+export { default as CreatePass } from "./CreateAccountH/CreatePass";
