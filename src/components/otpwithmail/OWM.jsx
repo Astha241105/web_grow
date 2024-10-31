0a2a309c277514f46551acdb257d7e2eaceae4a3
Astha241105 <astha241105@gmail.com>
2024-10-31 09:13:53 +0530

    changed css and corrected otp page

--- src/components/otpwithmail/OWM.jsx
+++ src/components/otpwithmail/OWM.jsx
@@ -1,21 +1,66 @@
-import React from 'react'
-import './OtpWithMail.css'
+import React, { useState } from 'react';
+import './OtpWithMail.css';
 
 const OtpWithMail = () => {
-  return (
-    <div  id="contout">
-      
-       <div id="container1">
-        <div id="codemail">Enter the code</div>
-       <p id="mess1">Enter the 4-digit OTP code we have sent to abc@gmail.com.</p>
-      <input className="boxes"></input>
-      <input className="boxes"></input>
-      <input className="boxes"></input>
-      <input className="boxes"></input><br></br>
-      <button className="verify1">Verify</button><br></br>
-      <div className="didnt1">
-      <span >Didn't receive the code?</span><span id="gradient1"> Resend code.</span></div></div></div>
-  )
-}
+  const [otp, setOtp] = useState(["", "", "", ""]);
 
-export default OtpWithMail
+  const handleChange = (e, index) => {
+    const { value } = e.target;
+
+
+    if (value.length > 1) return;
+
+    const newOtp = [...otp];
+    newOtp[index] = value;
+    setOtp(newOtp);
+
+
+    if (value && index < otp.length - 1) {
+      document.getElementById(`otp-input-${index + 1}`).focus();
+    }
+  };
+
+  const handleKeyDown = (e, index) => {
+    
+    if (e.key === 'Backspace' && !otp[index] && index > 0) {
+      document.getElementById(`otp-input-${index - 1}`).focus();
+    }
+  };
+
+  
+  const otpValue = otp.join("");
+
+  return (
+    <div id="contout">
+      <div id="container1">
+        <div id="codemail">Enter the code</div>
+        <p id="mess1">Enter the 4-digit OTP code we have sent to abc@gmail.com.</p>
+        
+        {otp.map((digit, index) => (
+          <input
+            key={index}
+            id={`otp-input-${index}`}
+            type="text"
+            className="boxes"
+            value={digit}
+            onChange={(e) => handleChange(e, index)}
+            onKeyDown={(e) => handleKeyDown(e, index)}
+            maxLength="1"
+          />
+        ))}
+
+        <br />
+        <button className="verify1" onClick={() => console.log("Entered OTP:", otpValue)}>
+          Verify
+        </button>
+        <br />
+        <div className="didnt1">
+          <span>Didn't receive the code?</span>
+          <span id="gradient1"> Resend code.</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default OtpWithMail;
