767422ebad8355d60cf0b76c01bd87ce2a6bd7f4
Astha241105 <astha241105@gmail.com>
2024-11-02 18:31:50 +0530

    added organization

--- src/components/CreateAccountH/Org_Des.jsx
+++ src/components/CreateAccountH/Org_Des.jsx
@@ -1,6 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { updateHostDetails } from "../store/slices/hostslice"; 
+import { useNavigate } from "react-router-dom";
 import "./Org_Des.css";
+
 const Org_Des = () => {
+  const [formData, setFormData] = useState({
+    organization: "",
+    designation: "",
+  });
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  
+  const existingHostDetails = useSelector((state) => state.host); 
+
+  
+  const handleInputChange = (e) => {
+    const { id, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [id]: value,
+    }));
+  };
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const updatedHostDetails = {
+      ...existingHostDetails,
+      ...formData,
+    };
+
+    dispatch(updateHostDetails(updatedHostDetails));
+
+  
+    navigate("/create-pass-host");
+  };
+
   return (
     <div className="forgot-pass">
       <img src="/Rectangle2.png" className="white-bg" alt="background" />
@@ -11,23 +50,39 @@ const Org_Des = () => {
         </div>
         <div className="form-section2">
           <h3 className="cnhead">Create your account</h3>
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="cnform2">
-              <label For="organisation" className="block mb-1">
+              <label htmlFor="organization" className="block mb-1">
                 Organisation:
               </label>
-              <select id="organisation">
-                <option value="Enter Organisation">Enter Organisation</option>
+              <select
+                id="organization"
+                value={formData.organization}
+                onChange={handleInputChange}
+                required
+              >
+                <option value="">Enter Organisation</option>
+                <option value="Organization A">Organization A</option>
+                <option value="Organization B">Organization B</option>
               </select>
               <img src="/down-arrow.svg" alt="arrow" className="arrow1" />
-              <label For="designation" className="block mb-1">
+
+              <label htmlFor="designation" className="block mb-1">
                 Designation:
               </label>
-              <select id="designation">
-                <option value="Enter Designation">Enter Designation</option>
+              <select
+                id="designation"
+                value={formData.designation}
+                onChange={handleInputChange}
+                required
+              >
+                <option value="">Enter Designation</option>
+                <option value="Manager">Manager</option>
+                <option value="Head">Head</option>
               </select>
               <img src="/down-arrow.svg" alt="arrow" className="arrow2" />
             </div>
+
             <button type="submit" className="org-continue">
               Continue
             </button>
@@ -44,4 +99,5 @@ const Org_Des = () => {
     </div>
   );
 };
+
 export default Org_Des;
