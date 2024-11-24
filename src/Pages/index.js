606c5b41a236c1abc1f2cc747bfcccca57bc55c3
gtanishka100 <gtanishka100@gmail.com>
2024-11-24 21:27:59 +0530

    Added route

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -3,3 +3,4 @@ export { default as Create_Events } from "./CreateEvents/Create_Event";
 export { default as HHomeScreen } from "./Host/HomeScreen.jsx";
 export { default as TeamManagement } from "./AddHost/AddHost.jsx";
 export { default as Event_Manage } from "./Manage/ManageEvents.jsx";
+export { default as QuizCreator } from "./Quiz/Quiz.jsx";
