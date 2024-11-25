6ff2e468d6d453e5614f64d413591c54f155b409
Astha241105 <astha241105@gmail.com>
2024-11-25 10:47:44 +0530

    added browse by category feature

--- src/browse-by-category/filter/filter.jsx
+++ src/browse-by-category/filter/filter.jsx
@@ -1,28 +1,38 @@
-import React from 'react'
-import "./filter.css"
+import React, { useState, useEffect } from 'react';
+import { selectedCategory, setSelectedCategory } from '../selected.js'; 
+import './filter.css';
 
-const Filter = () => {
+const EventType = () => {
+  const [category, setCategory] = useState(selectedCategory);
+
+  const handleCategoryChange = (event) => {
+    setCategory(event.target.value); 
+    setSelectedCategory(event.target.value); 
+  };
+
   return (
     <div id="filter-by">
       <div id="filter-by-1">
-      <select id="dropdown-event" defaultValue="">
-        <option value="hackathon" disabled>
-          Hackathon
-        </option>
-        <option value="seminar">Seminar</option>
-        <option value="webinar">Webinar</option>
-        <option value="quiz">Quiz</option>
-      </select>
-      <select id="dropdown-event" defaultValue="">
-        <option value="" disabled>
-          Event-type
-        </option>
-        <option value="offline">Offline</option>
-        <option value="online">Online</option>
-      </select>
+        <select
+          id="dropdown-event"
+          value={category} 
+          onChange={handleCategoryChange}
+        >
+          <option value="quizzes">Quizzes</option>
+          <option value="hackathons">Hackathons</option>
+          <option value="webinars">Webinars</option>
+          <option value="seminars">Seminars</option>
+        </select>
+        <select id="dropdown-event">
+          <option value="" disabled>
+            Event-type
+          </option>
+          <option value="offline">Offline</option>
+          <option value="online">Online</option>
+        </select>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Filter
+export default EventType;
--- src/component-2/browsebycategory/browsebycategory.jsx
+++ src/component-2/browsebycategory/browsebycategory.jsx
@@ -1,40 +1,69 @@
-import React from 'react'
-import "./browsebycategory.css"
-import i1 from "./quiz.png"
-import i2 from "./hack.png"
-import i3 from "./webinar.png"
-import i4 from "./meeting.png"
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { setSelectedCategory } from '../../browse-by-category/selected'; 
+import './browsebycategory.css';
+import i1 from './quiz.png';
+import i2 from './hack.png';
+import i3 from './webinar.png';
+import i4 from './meeting.png';
 
 const Browsebycategory = () => {
+  const navigate = useNavigate();
+
+  const handleViewMore = (category) => {
+    setSelectedCategory(category); 
+    navigate('/event-type'); 
+  };
+
   return (
     <div id="home-browse-by-category">
-        <div id="home-browse-by-category-head">Pick an <span id="home-browse-by-category-head-span">Opportunity</span> :</div>
-        <div  id="home-browse-by-category-content">
-            <div className='home-browse-by-category-content-box-1 home-browse-by-category-content-box'>
-                <img src={i1} className='home-browse-by-category-content-box-img'></img>
-                <div className='home-browse-by-category-content-box-name'>QUIZZES</div>
-                <button className="home-browse-by-category-content-box-button">View More</button>
-            </div>
-            <div className='home-browse-by-category-content-box-2 home-browse-by-category-content-box'>
-             <img src={i2} className='home-browse-by-category-content-box-img'></img>
-             <div className='home-browse-by-category-content-box-name'>HACKATHONS</div>
-                <button className="home-browse-by-category-content-box-button">View More</button>
-            </div>
-            <div className='home-browse-by-category-content-box-3 home-browse-by-category-content-box'>
-             <img src={i3} className='home-browse-by-category-content-box-img'></img>
-             <div className='home-browse-by-category-content-box-name'>WEBINARS</div>
-                <button className="home-browse-by-category-content-box-button">View More</button>
-            </div>
-            <div className='home-browse-by-category-content-box-4 home-browse-by-category-content-box'>
-             <img src={i4} className='home-browse-by-category-content-box-img'></img>
-             <div className='home-browse-by-category-content-box-name'>SEMINARS</div>
-                <button className="home-browse-by-category-content-box-button">View More</button>
-            </div>
-            
+      <div id="home-browse-by-category-head">
+        Pick an <span id="home-browse-by-category-head-span">Opportunity</span> :
+      </div>
+      <div id="home-browse-by-category-content">
+        <div className="home-browse-by-category-content-box-1 home-browse-by-category-content-box">
+          <img src={i1} className="home-browse-by-category-content-box-img" alt="Quiz" />
+          <div className="home-browse-by-category-content-box-name">QUIZZES</div>
+          <button
+            className="home-browse-by-category-content-box-button"
+            onClick={() => handleViewMore('quizzes')}
+          >
+            View More
+          </button>
         </div>
-      
+        <div className="home-browse-by-category-content-box-2 home-browse-by-category-content-box">
+          <img src={i2} className="home-browse-by-category-content-box-img" alt="Hackathons" />
+          <div className="home-browse-by-category-content-box-name">HACKATHONS</div>
+          <button
+            className="home-browse-by-category-content-box-button"
+            onClick={() => handleViewMore('hackathons')}
+          >
+            View More
+          </button>
+        </div>
+        <div className="home-browse-by-category-content-box-3 home-browse-by-category-content-box">
+          <img src={i3} className="home-browse-by-category-content-box-img" alt="Webinars" />
+          <div className="home-browse-by-category-content-box-name">WEBINARS</div>
+          <button
+            className="home-browse-by-category-content-box-button"
+            onClick={() => handleViewMore('webinars')}
+          >
+            View More
+          </button>
+        </div>
+        <div className="home-browse-by-category-content-box-4 home-browse-by-category-content-box">
+          <img src={i4} className="home-browse-by-category-content-box-img" alt="Seminars" />
+          <div className="home-browse-by-category-content-box-name">SEMINARS</div>
+          <button
+            className="home-browse-by-category-content-box-button"
+            onClick={() => handleViewMore('seminars')}
+          >
+            View More
+          </button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Browsebycategory
+export default Browsebycategory;
