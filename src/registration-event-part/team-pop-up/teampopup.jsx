ccc3bb07ca2dfa65d6e9be000d1bc9e944cc9227
Astha241105 <astha241105@gmail.com>
2024-11-28 21:12:13 +0530

    team-pop-up

---
+++ src/registration-event-part/team-pop-up/teampopup.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Teampopup = () => {
+  return (
+    <div id="team-ask-pop-up">
+      <div id="create-team">Create a team</div>
+      <div id="join-a-team">Join a team</div>
+    </div>
+  )
+}
+
+export default Teampopup
