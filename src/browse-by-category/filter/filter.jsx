6c1e421df35256d6b095eadc1fb887f3f38f7b88
Astha241105 <astha241105@gmail.com>
2024-11-28 21:57:54 +0530

    added fields

--- src/browse-by-category/filter/filter.jsx
+++ src/browse-by-category/filter/filter.jsx
@@ -40,28 +40,108 @@ const EventType = () => {
     <div id="filter-by">
       <div id="filter-by-1">
         <div id="select-it">
-          <select
-            id="dropdown-event"
-            value={selectedCategory}
-            onChange={handleCategoryChange}
-          >
-            <option value="">All Categories</option>
-            <option value="quiz">Quizzes</option>
-            <option value="hackathon">Hackathons</option>
-            <option value="webinar">Webinars</option>
-            <option value="seminar">Seminars</option>
-          </select>
-          <select
-            id="dropdown-event"
-            value={selectedType}
-            onChange={handleTypeChange}
-          >
-            <option value="">All Types</option>
-            <option value="offline">Offline</option>
-            <option value="online">Online</option>
-          </select>
+        <select
+          id="dropdown-event"
+          value={selectedCategory}
+          onChange={handleCategoryChange}
+        >
+          <option value="quiz">Quizzes</option>
+          <option value="hackathon">Hackathons</option>
+          <option value="webinar">Webinars</option>
+          <option value="seminar">Seminars</option>
+        </select>
+        <select
+          id="dropdown-event"
+          value={selectedType}
+          onChange={handleTypeChange}
+        >
+          <option value="offline">Offline</option>
+          <option value="online">Online</option>
+        </select></div>
+        <div id="one-event-details">
+      <div id="name-of-event-1">UX Hackathon</div>
+      <div id="one-event-details1">
+        <div id="institute">
+          <img
+            className="event-name-images-1"
+            src="/institute2.svg"
+            alt="college"
+          />
+          <div className="eventname-text-1">Ajay Kumar Garg Engineering College</div>
+        </div>
+        <div id="type-event">
+          <img
+            className="event-name-images-1"
+            src="Offline2.svg"
+            alt="event type"
+          />
+          <div className="eventname-text-1">Offline</div>
+        </div>
+        <div id="location">
+          <img
+            className="event-name-images-1"
+            src="/updated.svg"
+            alt="location"
+          />
+          <div id="event-details-updated-on">Updated on :</div>
+          <div className="eventname-text-1">Ghaziabad, India</div>
         </div>
       </div>
+      <div id="event-skill-0">
+        <div className="event-skill-1">Coding challenge</div>
+        <div className="event-skill-1">Social Media and Digital</div>
+      </div>
+      <div
+        id="register-for-the-event"
+        // onClick={handleRegisterClick}
+        style={{ cursor: "pointer" }}
+      >
+        Register
+      </div>
+    </div>
+    <div id="nav-card-event-page">
+    <div  id="registered1"className='part-details-info-1'>
+        <img className="part-details-img" src="/people.svg" alt="Registered Icon" />
+        <div className="minor-details">
+        <div  >Registered Candidates</div>
+        <div>400</div></div>
+      </div>
+      <div id="team1" className='part-details-info-1'>
+        <img className="part-details-img" src="/team.svg" alt="Team Size Icon" />
+        <div  className="minor-details">
+        <div>Team Size</div>
+        <div>400</div></div>
+      </div>
+      <div id="deadline1" className='part-details-info-1'>
+        <img className="part-details-img" src="/deadline.svg" alt="Deadline Icon" />
+        <div className="minor-details">
+        <div  >Deadline</div>
+        <div>400</div></div>
+      </div>
+    </div>
+    <div id="this-div-has-all-info">
+     <div  className="this-div-has-all-info-parts">
+        <div  className="this-div-has-all-info-heads">Introduction</div>
+            <div className="this-div-has-all-info-info"></div>
+        </div>
+        <div  className="this-div-has-all-info-parts">
+        <div  className="this-div-has-all-info-heads">Team & Eligibility</div>
+            <div className="this-div-has-all-info-info"></div>
+        </div>
+        <div  className="this-div-has-all-info-parts">
+        <div  className="this-div-has-all-info-heads">Timeline</div>
+            <div className="this-div-has-all-info-info"></div>
+        </div>
+        <div  className="this-div-has-all-info-parts">
+        <div  className="this-div-has-all-info-heads">Venue</div>
+            <div className="this-div-has-all-info-info"></div>
+        </div>
+        <div  className="this-div-has-all-info-parts">
+        <div  className="this-div-has-all-info-heads">Coins</div>
+            <div className="this-div-has-all-info-info"></div>
+        </div></div>
+      </div>
+
 
       <div id="filtered-events">
         {filteredEvents.length > 0 ? (
